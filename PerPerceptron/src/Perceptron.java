234b4161af756235c99f21244ac3cccd95d9d103
Lucas Bernardo <lucasbernardo95@gmail.com>
2016-10-28 20:33:04 -0300

    Proojeto finalizado.

---
+++ PerPerceptron/src/Perceptron.java
@@ -0,0 +1,63 @@
+
+/**
+ * @author lber
+ */
+public class Perceptron {
+
+    int[][] entradas;
+    int saidas[];
+    int pesos[];
+    double aprendizagem = 2;
+
+    public Perceptron(int entradas[][], int saidas[], int pesos[]) {
+        this.entradas = entradas;
+        this.saidas = saidas;
+        this.pesos = pesos;
+    }
+
+    public void treinar() {
+        int saida = 0;
+        for (int i = 0; i < entradas.length; i++) {//entradas.length corresponde às 6 linhas das entradas
+            saida = ativacao(i);
+            //e = Yd – Y 
+            int erro = (saidas[i] - saida);
+            //Caso a saída seja diferente da esperada, recalcula os pesos e repete a volta no laço
+            if (saida != saidas[i]) {
+                //envia o índice da entrada e o valor do erro que foi calculado
+                calcularPesos(i, erro);
+                //repete a volta
+                --i;
+            } else {//caso a saída seja o valor que era esperado 
+
+                //printa a coluna com os valores corretos
+                System.out.print("Entradas: ");
+                for (int j = 0; j < 4; j++) {
+                    System.out.print(" " + entradas[i][j]);
+                }
+                System.out.println(" Saída esperada: " + saidas[i] + " Saída obtida: " + saida);
+            }
+        }
+
+    }
+
+    public void calcularPesos(int p, int erro) {
+        for (int i = 0; i < 4; i++) {
+            pesos[i] = (int) (pesos[i] + aprendizagem * erro * entradas[i][i]);
+            //System.out.println("novo peso " + i + " = " + (pesos[i] + aprendizagem * erro * entradas[i][i]));
+        }
+    }
+
+    // Método responsável pelo somatório e a função de ativação.
+    public int ativacao(int indice) {
+        //variável para armazenar o somatório
+        int soma = 0;
+        for (int i = 0; i < 4; i++) {
+            soma += (pesos[i] * entradas[indice][i]);
+        }
+        /**
+         * Se o sinal for forte, ou seja, maior que o limite estabelecido, deixa
+         * o sinal passar caso contrário, não deixa.
+         */
+        return (soma >= 0) ? 1 : 0;//se for maior retorna 1, se não retorna 0
+    }
+}
---
+++ PerPerceptron/src/Principal.java
@@ -0,0 +1,26 @@
+/**
+ * @author lber
+ */
+public class Principal {
+    public static void main(String[] args) {
+        //Variável com valores das entradas
+        int entradas[][] = { 
+            {1,0,1,0}, 
+            {0,1,0,1}, 
+            {1,0,1,0},
+            {0,1,1,1},
+            {0,0,1,1},
+            {0,0,0,1} 
+        };
+        //valores esperados como saída para cada linha de entrada
+        int saidas[] = {0,1,0,1,0,1};
+        
+        //Valores dos pesos para cada entrada
+        int pesos[] = {-1,1,-1,1};
+        
+        //Seta os valores de entradas, saídas esperadas para cada entrada, e os pesos para cada valor de entrada 
+        Perceptron p = new Perceptron(entradas, saidas, pesos);
+        //inicia o processo de aprendizagem da rede
+        p.treinar();
+    }
+}
